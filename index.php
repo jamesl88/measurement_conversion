833895695d766b548802097226c1aea4c618a59c
James Lieu <j.lieu888@gmail.com>
2016-07-27 22:36:48 +0100

    Add index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Measurement Conversion</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <div id="main-content">
+      <h1>Measurement Conversion</h1>
+
+      <ul id="measurement-types">
+        <li><a href="length.php">Length and distance</a></li>
+        <li><a href="area.php">Area</a></li>
+        <li><a href="volume.php">Volume and Capacity</a></li>
+        <li><a href="mass.php">Mass and Weight</a></li>
+        <li><a href="speed.php">Speed</a></li>
+        <li><a href="temperature.php">Temperature</a></li>
+      </ul>
+    </div>
+  </body>
+</html>
